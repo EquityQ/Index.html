0456cfeb8070feed3dc990e319d35ffe301b905e
EquityQ <87411060+EquityQ@users.noreply.github.com>
2021-08-09 16:15:57 +0800

    Delete file directory

--- file/cookie.js
+++
@@ -1,7 +0,0 @@
-var p = $.cookie("user");
-if (p != undefined && p!= "default"){
-    document.getElementById("lig").innerHTML = p;
-}
-else{
-    document.getElementById("lig").innerHTML = '账号登录';
-}
